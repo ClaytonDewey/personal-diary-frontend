4c1fee4cdb6e6ee205ca59b8f6fad90a8a5f4085
Clay <clay@dryadmedia.com>
2022-01-26 11:44:31 -0600

    Update package.json
    Copied from the Sitepoint page in order to follow along with their tutorial.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
